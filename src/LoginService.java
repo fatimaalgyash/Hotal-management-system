155e5e6b590d2a099af068bcbe3068d59e1a12cf
fatima <fatimaalgyash@gmail.com>
2026-06-25 23:49:38 +0200

    initial project upload and added javadoc

---
+++ src/LoginService.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+/**
+ * This class handles user login.
+ */
+public class LoginService implements Loginable {
+
+    /**
+     * Checks username and password from database.
+     *
+     * @param username user name
+     * @param password user password
+     * @return true if login is successful, false otherwise
+     */
+    @Override
+    public boolean login(String username, String password) {
+
+        try {
+            Connection con = DBConnection.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT * FROM users WHERE username=? AND password=?"
+            );
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            return rs.next();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
