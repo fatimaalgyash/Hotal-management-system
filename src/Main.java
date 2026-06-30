f1afaf35c34a9cf1d4922afbc7ca6d2693b77153
fatima <fatimaalgyash@gmail.com>
2026-06-30 03:43:12 +0200

    project upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+@SuppressWarnings("all")
 public class Main {
-
     public static void main(String[] args) {
-
+        // تشغيل واجهة تسجيل الدخول الخاصة بنظام الفندق
         new LoginGUI();
     }
 }
